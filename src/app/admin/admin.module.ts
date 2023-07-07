c376698aa3b86d918598e5e7ef90e276b2d6442d
Manoj M <manumadhu1425@gmail.com>
2023-07-07 12:45:46 +0530

    Navigation component for admin module added

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,13 +3,14 @@ import {CommonModule} from '@angular/common';
 import {AdminProfileComponent} from './components/admin-profile/admin-profile.component';
 import {AdminComponent} from './admin.component';
 import {RouterModule} from "@angular/router";
-import {AdminService} from "./services/admin.service";
+import {NavigationBarComponent} from './components/navigation-bar/navigation-bar.component';
 
 
 @NgModule({
   declarations: [
     AdminProfileComponent,
-    AdminComponent
+    AdminComponent,
+    NavigationBarComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/admin/components/navigation-bar/navigation-bar.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-bar',
+  templateUrl: './navigation-bar.component.html',
+  styleUrls: ['./navigation-bar.component.css']
+})
+export class NavigationBarComponent implements OnInit {
+
+  isClose: boolean = true;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  toggleNav() {
+    this.isClose = !this.isClose;
+  }
+
+
+}
